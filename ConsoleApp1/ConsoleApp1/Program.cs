4f86bd3fd9b09934f8dfe2f5e7a06e3888f2ed15
visualstudio <visualstudio@230-PC37>
2019-04-11 16:05:06 -0400

    Section C (4:04 commit)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,28 +13,42 @@ namespace ConsoleApp1
             string beowulfTxt = System.IO.File.ReadAllText(@"U: \Users\732160\peterWeek6\peter week6\Beowulf.txt");
             Console.WriteLine(beowulfTxt);
             Console.WriteLine("===============================================================");
-            Console.WriteLine("===============================================================");
+            Console.WriteLine("==============================Hit Enter=================================");
             Console.ReadLine();
 
             int countingLines = System.IO.File.ReadLines(@"U: \Users\732160\peterWeek6\peter week6\Beowulf.txt").Count();
             Console.WriteLine("Total number of lines in the text is "+countingLines);
-            Console.WriteLine("===============================================================");
+            Console.WriteLine("===============================Hit Enter================================");
             Console.ReadLine();
 
             int wordCount = beowulfTxt.Split().Length;
             Console.WriteLine("Toal number of words in the text file is "+wordCount);
-            Console.WriteLine("===============================================================");
+            Console.WriteLine("==============================Hit Enter=================================");
             Console.ReadLine();
 
             int characterCount = beowulfTxt.Length;
             Console.WriteLine("Toal number of letters in the text file is " + characterCount);
-            Console.WriteLine("===============================================================");
+            Console.WriteLine("===============================Hit Enter================================");
             Console.ReadLine();
 
             double lPerWord;
             lPerWord = (double)characterCount / (double)wordCount;
             Console.WriteLine("Average number of letters per word is " + lPerWord);
-            Console.WriteLine("===============================================================");
+            Console.WriteLine("===============================Hit Enter================================");
+            Console.ReadLine();
+
+            string [] beowulfTxtArray = System.IO.File.ReadAllLines(@"U: \Users\732160\peterWeek6\peter week6\Beowulf.txt");
+            int lineCount = 0;
+
+            for (int i =1; i < beowulfTxtArray.Length; i++)
+            {
+                if (beowulfTxtArray[i].ToUpper().Contains("SEA") && beowulfTxtArray[i].ToUpper().Contains("FARE"))
+                {
+                    lineCount++;
+                }
+            }
+            Console.WriteLine("Toal number of lines that contains word --Sea-- and --Fare-- " + lineCount);
+            Console.WriteLine("==============================Hit Enter=================================");
             Console.ReadLine();
         }
     }
