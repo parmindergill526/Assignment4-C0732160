bc5a5081b487f0d188b4230f83aee5ecf698e4b0
visualstudio <visualstudio@230-PC37>
2019-04-11 14:40:59 -0400

    section B (2:40 commit)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,9 +13,17 @@ namespace ConsoleApp1
             string beowulfTxt = System.IO.File.ReadAllText(@"U: \Users\732160\peterWeek6\peter week6\Beowulf.txt");
             Console.WriteLine(beowulfTxt);
             Console.WriteLine("===============================================================");
+            Console.WriteLine("===============================================================");
             Console.ReadLine();
-            var countingLines = System.IO.File.ReadLines(@"U: \Users\732160\peterWeek6\peter week6\Beowulf.txt").Count();
+
+            int countingLines = System.IO.File.ReadLines(@"U: \Users\732160\peterWeek6\peter week6\Beowulf.txt").Count();
             Console.WriteLine("Total number of lines in the text is "+countingLines);
+            Console.WriteLine("===============================================================");
+            Console.ReadLine();
+
+            int wordCount = beowulfTxt.Split().Length;
+            Console.WriteLine("Toal number of words in the text file is "+wordCount);
+            Console.WriteLine("===============================================================");
             Console.ReadLine();
         }
     }
